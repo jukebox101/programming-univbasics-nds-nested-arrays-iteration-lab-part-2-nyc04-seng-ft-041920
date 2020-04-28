e10c6e7f5513db38d022e19b7d1723bb76dddb58
Leonora Squires <leonora.squires@gmail.com>
2020-04-28 20:40:03 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,8 +7,8 @@ while row_index < src.count do
   element_index = 0
   min = 100000000
   while element_index < src[row_index].count do
-    if 
-      
+    if min > src[row_index][element_index]
+      min = src[row_index][element_index]
     end
     element_index += 1
   end
